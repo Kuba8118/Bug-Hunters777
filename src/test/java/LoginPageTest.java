a047303c4f207604932be9fe4aa672b8fe61f300
Kuba8118 <kubaiskenderov@gmail.com>
2025-01-27 01:06:16 +0600

    commit

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -48,6 +48,7 @@ public void wrongLoginTest() {
 
         boolean isNullPasswordErrorDisplayed = loginPage.IncorrectPasswordMessage();
         Assert.assertTrue(isNullPasswordErrorDisplayed);
+
     }
 
 
