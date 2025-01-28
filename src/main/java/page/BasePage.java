70a42ebfbd2efcd6f451a3dd2371a0c4be1dbf93
Agu <kanvolvaak94@gmail.com>
2025-01-28 15:19:03 +0600

    Добавлена папка .idea в .gitignore и добавлены страницы для добавления курсов

--- src/main/java/page/BasePage.java
+++ src/main/java/page/BasePage.java
@@ -5,7 +5,7 @@ import driver.Driver;
 import helper.WebElementActions;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class  BasePage {
+public abstract class BasePage {
     public WebElementActions webElementActions = new WebElementActions();
 
     public BasePage(){
--- src/main/java/page/dashboard/DashboardPage.java
+++ src/main/java/page/dashboard/DashboardPage.java
@@ -3,6 +3,7 @@ package page.dashboard;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import page.BasePage;
+import page.coursesPage.CreateCoursePage;
 import page.users.AddUserPage;
 
 public class DashboardPage extends BasePage {
@@ -10,10 +11,22 @@ public class DashboardPage extends BasePage {
 
     @FindBy(xpath = "(//a[normalize-space()='Add user'])[2]")
     public WebElement addUserBtn;
+
     @FindBy(css = ".profile-menu-button")
     public WebElement profileButton;
+
     @FindBy(xpath = "//p[text()='Go to legacy interface']")
     public WebElement dashboardButton;
+
+    @FindBy(css = "#tl-admin-courses .tl-bold-link")
+    public WebElement coursesButton;
+
+    @FindBy(xpath = "//div[@id='tl-admin-courses']")
+    public WebElement coursesPageLink;
+
+    @FindBy(xpath = "//a[contains(@class, 'btn-primary') and text()='Add course']")
+    public WebElement addCoursePageLink;
+
 
     public AddUserPage navigateToAddUserPage() {
         webElementActions.click(addUserBtn);
@@ -21,12 +34,19 @@ public class DashboardPage extends BasePage {
 
     }
 
+    public CreateCoursePage navigateToCoursesPage(){
+        webElementActions.click(coursesButton);
+        return new CreateCoursePage();
+    }
+
+    public CreateCoursePage navigateToAddCoursePage(){
+        webElementActions.click(addCoursePageLink);
+        return new CreateCoursePage();
+    }
+
     public DashboardPage mooveToProfile() {
         profileButton.click();
         dashboardButton.click();
         return this;
     }
-
-
-
 }
